d24473dbe8115e468f4f24c63605c729a01d809e
shurigan <shurigan@gmail.com>
2013-11-25 08:03:41 +0400

    add schema

--- app/schema.sql
+++ app/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `temewebtw`.`results` (
-  `id` INT NOT NULL,
-  `url` VARCHAR(255) NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL  AUTO_INCREMENT,
+  `url` VARCHAR(255) NOT NULL,
   `result_string` TEXT NULL,
   `total` INT NULL,
   PRIMARY KEY (`id`));
